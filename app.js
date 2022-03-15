0ab308a99707093a6d08d9977a704d5271b6eafd
alireza koohzad <alirezakoohzad2705@gmail.com>
2022-03-15 22:50:29 +0330

    initial project and add some packages

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const sequelize = require('./utils/database');
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req , res , next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods' , 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 
 
-app.listen(3000);
+
+sequelize
+    .sync()
+    .then(() =>{
+        app.listen(8080);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+
+
